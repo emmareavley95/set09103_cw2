b819dec4db57213b527240ed668a5f90fa81a786
Emma Reavley <emma.reavley@wanadoo.fr>
2016-11-11 13:40:39 +0000

    create login or signup page

--- src/app.py
+++ src/app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def root():
-  render_template('index.html'), 200
+  if request.method == 'POST':
+    print request.form
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    return render_template('index.html', name=name, email=email), 200
+  else:
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
