b487480ec9b5ebf272a150d7b192105e22a67a2c
Emma Reavley <emma.reavley@wanadoo.fr>
2016-11-17 19:57:52 +0000

    add log out functionnality and flash messages

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, session, flash
 app = Flask(__name__)
+app.secret_key = 'sGJeN21Z]=6RmK='
 
 @app.route('/', methods=['GET','POST'])
 def root():
@@ -23,8 +24,16 @@ def login():
     if request.form['username'] != 'admin' or request.form['password'] != 'simon_AWT':
       error = 'Incorrect Username or Password, Please try again.'
     else:
+      session['logged_in'] = True
+      flash('Welcome {{ username }}! You can now send your wishlist to Santa')
       return redirect(url_for('index'))
   return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+  session.pop('logged_in', None)
+  flash('Successful log out')
+  return redirect(url_for('root'))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
