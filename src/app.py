f39298c809e5d5d157839ab0fcd1af921e8baa17
Emma Reavley <emma.reavley@wanadoo.fr>
2016-11-17 18:39:57 +0000

    create login page for members

--- src/app.py
+++ src/app.py
@@ -10,7 +10,21 @@ def root():
     password = request.form['password']
     return render_template('index.html', username=username, email=email), 200
   else:
-    return render_template('login.html')
+    return render_template('home.html')
+
+@app.route('/index')
+def index():
+  return render_template('index.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+  error = None
+  if request.method == 'POST':
+    if request.form['username'] != 'admin' or request.form['password'] != 'simon_AWT':
+      error = 'Incorrect Username or Password, Please try again.'
+    else:
+      return redirect(url_for('index'))
+  return render_template('login.html', error=error)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
